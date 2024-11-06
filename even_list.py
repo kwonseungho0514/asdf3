ddde8cbbe8bcd48cf494d212694c47ec9b9d73d4
kwonseungho0514 <paulkwon0514@gmail.com>
2024-11-06 03:41:55 -0800

    even_list finished

---
+++ even_list.py
@@ -0,0 +1,8 @@
+def even_list(int_list: List[int]) -> List[int]:
+    result = []
+
+    for element in int_list:
+        if(element % 2 == 0):
+            result.append(element)
+
+    return result
